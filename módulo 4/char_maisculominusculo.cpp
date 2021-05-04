213fb4d5e6fb26df5afe7f8fb138565bdcb8660f
tanjilkh <tanjilkh@gmail.com>
2021-05-04 12:07:59 +0100

    imprimir codigo do char

---
+++ módulo 4/char_maisculominusculo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char c1 = 'a';
+    char c2 = 'A';
+
+
+    cout << c1 << '|' << c2 << endl;
+    cout << (int) c1;
+    cout << (int)
+}
