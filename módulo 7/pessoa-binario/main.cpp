0f11573929ad2e08b9e212802c568b391b0656db
tanjilkh <tanjilkh@gmail.com>
2021-05-27 10:04:42 +0100

    pessoa binaria

--- módulo 7/pessoa-binario/main.cpp
+++ módulo 7/pessoa-binario/main.cpp
@@ -14,7 +14,26 @@ int main(){
     p1.imprimir();
     
     ofstream outfile;
-    outfile.open("teste.dat", ios::binary);
+    
+    outfile.open("p1.dat", ios::binary);
     outfile.write( (const char *)(&p1), sizeof(pessoa) );
     outfile.close();
+
+    pessoa p2;
+    
+    p2.idade = 10;
+    ofstream outfilep2;
+    outfilep2.open("p2.dat", ios::binary);
+    outfilep2.write( (const char *)(&p2), sizeof(pessoa));
+    outfilep2.close();
+
+    ifstream infile;
+    
+    infile.open("p2.dat", ios::binary);
+    infile.read( (char *)(&p2), sizeof(pessoa));
+    while(!infile.eof()){
+        cout << &p2 << endl;
+        infile.read((char *)(&p2), sizeof(pessoa));
+    }
+    infile.close();
 }
